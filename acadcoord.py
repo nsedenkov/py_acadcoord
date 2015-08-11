568786a21068a028cc788e721146d575c8fe87aa
N.Sedenkov <nsedenkov@yandex.ru>
2015-08-11 09:50:23 +0300

    2015-08-11 square done

--- acadcoord.py
+++ acadcoord.py
@@ -13,8 +13,8 @@
 from __future__ import division
 import sys
 from os import path, environ
-if path.exists(path.normpath('x:\СИД\~INFO\Python\COMLibs').decode('utf-8').encode('cp1251')):
-    sys.path.append(path.normpath('x:\СИД\~INFO\Python\COMLibs').decode('utf-8').encode('cp1251'))
+if path.exists(path.normpath(u'x:\СИД\~INFO\Python\COMLibs')):
+    sys.path.append(path.normpath(u'x:\СИД\~INFO\Python\COMLibs'))
 if path.exists(path.normpath('d:\PY\LIB')):
     sys.path.append(path.normpath('d:\PY\LIB'))
 from Tkinter import Tk, Button, Frame, LabelFrame, Listbox, Scrollbar, Menu, Menubutton, Radiobutton, Label, Entry, StringVar, IntVar
@@ -100,10 +100,10 @@ class main:
                                  2:u'По РДС 30-201-98'
                                 }
         self.DicAcadErrors = {
-                                1:'AutoCAD не запущен',
-                                2:'В AutoCAD нет открытых чертежей'
+                                1:u'AutoCAD не запущен',
+                                2:u'В AutoCAD нет открытых чертежей'
                              }
-        self.title = 'Экспорт координат объектов AutoCAD'
+        self.title = u'Экспорт координат объектов AutoCAD'
         self.master.title(self.title)
         self.master.geometry('360x330')
         self.master.resizable(False, False)
@@ -120,19 +120,19 @@ class main:
         self.master.LFrame.pack(side = 'left', fill = 'y')
         self.master.RFrame = Frame(self.master)
         self.master.RFrame.pack(side = 'right', fill = 'both')
-        self.master.frame1=LabelFrame(self.master.RFrame, height = 80, labelanchor='nw', text='Слои чертежа')
+        self.master.frame1=LabelFrame(self.master.RFrame, height = 80, labelanchor='nw', text=u'Слои чертежа')
         self.master.frame1.pack(side = 'top', fill = 'x', expand = 0, padx=2)
         self.master.frame3=Frame(self.master.RFrame, height = 80)
         self.master.frame3.pack(side = 'bottom', fill = 'x', expand = 0)
-        self.master.frame4=LabelFrame(self.master.LFrame, width = 220, labelanchor='nw', text='Порядок нумерации')
+        self.master.frame4=LabelFrame(self.master.LFrame, width = 220, labelanchor='nw', text=u'Порядок нумерации')
         self.master.frame4.pack(side = 'top', fill = 'both', expand = 1, padx=2)
         self.master.frame5=Frame(self.master.LFrame, height = 80)
         self.master.frame5.pack(side = 'top', fill = 'x', expand = 0)
-        self.master.frame6 = LabelFrame(self.master.RFrame, height = 80, labelanchor='nw', text='Надписи для масштаба:')
+        self.master.frame6 = LabelFrame(self.master.RFrame, height = 80, labelanchor='nw', text=u'Надписи для масштаба:')
         self.master.frame6.pack(side = 'bottom', fill = 'x', expand = 0, padx=2)
-        self.master.frame7 = LabelFrame(self.master.RFrame, height = 80, labelanchor='nw', text='Вид ведомости:')
+        self.master.frame7 = LabelFrame(self.master.RFrame, height = 80, labelanchor='nw', text=u'Вид ведомости:')
         self.master.frame7.pack(side = 'bottom', fill = 'x', expand = 0, padx=2)
-        self.master.frame2=LabelFrame(self.master.RFrame, labelanchor='nw', text='Объекты слоя')
+        self.master.frame2=LabelFrame(self.master.RFrame, labelanchor='nw', text=u'Объекты слоя')
         self.master.frame2.pack(side = 'top', fill = 'both', expand = 1)
 
         self.master.layers = Menubutton(self.master.frame1, indicatoron = 1, anchor = 'w', textvariable = self.LayrVar)
@@ -155,8 +155,8 @@ class main:
             self.rmenu.add_command(label = self.DicReports[x], command = lambda x = x: self.SetActiveReport(x))
         self.RepVar.set(self.rmenu.entrycget(0, 'label'))
         
-        self.master.btn1 = Button(self.master.frame3, width = 16, height = 1, text = 'Экспорт', state=DISABLED, command=self.btn1_press)
-        self.master.btn2 = Button(self.master.frame3, width = 16, height = 1, text = 'Закрыть', command=self.Quit)
+        self.master.btn1 = Button(self.master.frame3, width = 16, height = 1, text = u'Экспорт', state=DISABLED, command=self.btn1_press)
+        self.master.btn2 = Button(self.master.frame3, width = 16, height = 1, text = u'Закрыть', command=self.Quit)
         self.master.btn1.pack(anchor = 'n', side = 'left', padx = 2, pady = 2, expand = 1)
         self.master.btn2.pack(anchor = 'n', side = 'right', padx = 2, pady = 2, expand = 1)
         self.master.scrl=Scrollbar(self.master.frame2)
@@ -169,9 +169,9 @@ class main:
         self.master.sf3=Frame(self.master.frame4)
         self.master.sf4=Frame(self.master.frame4)
         self.master.sf5=Frame(self.master.frame4)
-        self.master.sf6=LabelFrame(self.master.frame5, height = 80, width = 220, labelanchor='nw', text='Префикс нумерации')
-        self.master.sf7=LabelFrame(self.master.frame5, height = 80, width = 220, labelanchor='nw', text='Нумерация точек с:')
-        self.master.sf8=LabelFrame(self.master.frame5, height = 80, width = 220, labelanchor='nw', text='Нумерация участков с:')
+        self.master.sf6=LabelFrame(self.master.frame5, height = 80, width = 220, labelanchor='nw', text=u'Префикс нумерации')
+        self.master.sf7=LabelFrame(self.master.frame5, height = 80, width = 220, labelanchor='nw', text=u'Нумерация точек с:')
+        self.master.sf8=LabelFrame(self.master.frame5, height = 80, width = 220, labelanchor='nw', text=u'Нумерация участков с:')
         self.master.sf1.pack(pady = 1, fill = 'x')
         self.master.sf2.pack(pady = 1, fill = 'x')
         self.master.sf3.pack(pady = 1, fill = 'x')
@@ -180,11 +180,11 @@ class main:
         self.master.sf6.pack(side = 'bottom', fill = 'both', expand = 1, padx=2, pady = 2)
         self.master.sf7.pack(side = 'bottom', fill = 'x', expand = 0, padx=2, pady = 2)
         self.master.sf8.pack(side = 'bottom', fill = 'x', expand = 0, padx=2, pady = 2)
-        self.master.rb1=Radiobutton(self.master.sf1, text = 'По умолчанию', variable=self.RBVar, value=1)
-        self.master.rb2=Radiobutton(self.master.sf2, text = 'Север - юг', variable=self.RBVar, value=2)
-        self.master.rb3=Radiobutton(self.master.sf3, text = 'Запад - восток', variable=self.RBVar, value=3)
-        self.master.rb4=Radiobutton(self.master.sf4, text = 'Юг - север', variable=self.RBVar, value=4)
-        self.master.rb5=Radiobutton(self.master.sf5, text = 'Восток - запад', variable=self.RBVar, value=5)
+        self.master.rb1=Radiobutton(self.master.sf1, text = u'По умолчанию', variable=self.RBVar, value=1)
+        self.master.rb2=Radiobutton(self.master.sf2, text = u'Север - юг', variable=self.RBVar, value=2)
+        self.master.rb3=Radiobutton(self.master.sf3, text = u'Запад - восток', variable=self.RBVar, value=3)
+        self.master.rb4=Radiobutton(self.master.sf4, text = u'Юг - север', variable=self.RBVar, value=4)
+        self.master.rb5=Radiobutton(self.master.sf5, text = u'Восток - запад', variable=self.RBVar, value=5)
         self.master.rb1.pack(side = 'left', padx = 2, fill = 'x')
         self.master.rb2.pack(side = 'left', padx = 2, fill = 'x')
         self.master.rb3.pack(side = 'left', padx = 2, fill = 'x')
@@ -287,24 +287,19 @@ class main:
                 for i in xrange(1,8):
                     ws.Cells[row+4, i].Borders[4].LineStyle = 1
                     ws.Cells[row+4, i].Borders[4].Weight = 3
-                st = u'элемент'
-                ws.Cells[row+1, 2] = st.encode('cp1251')
+                ws.Cells[row+1, 2] = u'элемент'
                 ws.Cells[row+1, 2].HorizontalAlignment = 2
-                st = u'прямая'
-                ws.Cells[row+1, 3] = st.encode('cp1251')
+                ws.Cells[row+1, 3] = u'прямая'
                 ws.Cells[row+1, 3].HorizontalAlignment = 2
-                st = u'дирекционное направление'
-                ws.Cells[row+2, 2] = st.encode('cp1251')
+                ws.Cells[row+2, 2] = u'дирекционное направление'
                 ws.Cells[row+2, 2].HorizontalAlignment = 2
                 ws.Cells[row+2, 3] = u'%(deg)02d° %(mnt)02d\' %(dmnt)02d\"' % {'deg':ang[0], 'mnt':ang[1], 'dmnt':ang[2]/100*60}
                 ws.Cells[row+2, 3].HorizontalAlignment = 2
-                st = u'расстояние'
-                ws.Cells[row+3, 2] = st.encode('cp1251')
+                ws.Cells[row+3, 2] = u'расстояние'
                 ws.Cells[row+3, 2].HorizontalAlignment = 2
                 ws.Cells[row+3, 3] = '{0:10.2f}'.format(l)
                 ws.Cells[row+3, 3].HorizontalAlignment = 2
-                st = u'точка'
-                ws.Cells[row+4, 2] = st.encode('cp1251')
+                ws.Cells[row+4, 2] = u'точка'
                 ws.Cells[row+4, 2].HorizontalAlignment = 2
                 ws.Cells[row+4, 3] = nmb[1]
                 ws.Cells[row+4, 3].HorizontalAlignment = 2
@@ -332,26 +327,20 @@ class main:
         
     def XlsHdrString(self, ws, row, mode):
         if mode < 2:
-            st = u'Номер точки'
-            ws.Cells[row, 1] = st.encode('cp1251')
+            ws.Cells[row, 1] = u'Номер точки'
             ws.Cells[row, 1].BorderAround(1,3,1,1)
             ws.Cells[row, 1].HorizontalAlignment = 3
-            st = u'X, м'
-            ws.Cells[row, 2] = st.encode('cp1251')
+            ws.Cells[row, 2] = u'X, м'
             ws.Cells[row, 2].BorderAround(1,3,1,1)
-            st = u'Y, м'
-            ws.Cells[row, 3] = st.encode('cp1251')
+            ws.Cells[row, 3] = u'Y, м'
             ws.Cells[row, 3].BorderAround(1,3,1,1)
             if mode == 0:
-                st = u'Длина, м'
-                ws.Cells[row, 4] = st.encode('cp1251')
+                ws.Cells[row, 4] = u'Длина, м'
                 ws.Cells[row, 4].BorderAround(1,3,1,1)
-                st = u'Дирекционный угол'
-                ws.Cells[row, 5] = st.encode('cp1251')
+                ws.Cells[row, 5] = u'Дирекционный угол'
                 ws.Cells[row, 5].BorderAround(1,3,1,1)
         elif mode == 2:
-            st = u'№ точки'
-            ws.Cells[row, 1] = st.encode('cp1251')
+            ws.Cells[row, 1] = u'№ точки'
             ws.Cells[row, 1].BorderAround(1,3,1,1)
             ws.Cells[row, 1].HorizontalAlignment = 3
             ws.Cells[row, 2].Borders[3].LineStyle = 1
@@ -405,21 +394,25 @@ class main:
         ROW = 0
         if self.DicReports.values().index(self.RepVar.get()) == 2:
             ROW += 1
-            st = u'ВЕДОМОСТЬ КООРДИНАТ ТОЧЕК ГРАНИЦЫ ТЕХНИЧЕСКОЙ ЗОНЫ ЛИНЕЙНОГО ОБЪЕКТА'
-            S.Cells[ROW, 1] = st.encode('cp1251')
+            S.Cells[ROW, 1] = u'ВЕДОМОСТЬ КООРДИНАТ ТОЧЕК ГРАНИЦЫ ТЕХНИЧЕСКОЙ ЗОНЫ ЛИНЕЙНОГО ОБЪЕКТА'
         lxy = [] # Сквозная нумерация точек
         pxy = [] # Список точек внутри участка для исключения дублирования строк в ведомости
         i = 1 # Порядковый номер участка
         for crdlst in self.PLineCrd:
             ROW += 1
-            st = 'Участок '
-            S.Cells[ROW, 1] = st.decode('utf-8').encode('cp1251') + self.nprefix + str(i + self.startnumprclfrom - 1)
+            st = u'Участок '
+            S.Cells[ROW, 1] = st + self.nprefix + str(i + self.startnumprclfrom - 1)
+            ROW += 1
+            st = u'Площадь '
+            s1 = str(trunc(crdlst['sq']))
+            s2 = '{0:'+str(len(s1))+'.0f}'
+            S.Cells[ROW, 1] = st + s2.format(crdlst['sq']) + u' кв.м.'
             self.MarkParcel(crdlst, self.nprefix + str(i + self.startnumprclfrom - 1), 'Numbers')
             ROW += 1
             ROW = self.XlsHdrString(S, ROW,  self.DicReports.values().index(self.RepVar.get()))
             del pxy[0:len(pxy)]
             j = 1
-            for txy in crdlst:
+            for txy in crdlst['crd']:
                 if lxy.count(txy) == 0:
                     lxy.append(txy)
                     if len(str(lxy.index(txy)+self.startnumpntfrom)) > 3:
@@ -510,7 +503,8 @@ class main:
     def ResetCoord(self):
         del self.PLineCrd[0:len(self.PLineCrd)]
         
-    def CollectCoord(self, coords):
+    def CollectCoord(self, sq, coords):
+        dic_pl = {}
         tmplst = []
         if (len(coords) % 2 == 0):
             if (len(coords) % 3 > 0):
@@ -534,23 +528,28 @@ class main:
         for j in xrange(0,len(crdx)):
             txy = (round(crdx[j],2), round(crdy[j],2))
             tmplst.append(txy)
-        self.PLineCrd.append(tmplst)
+        dic_pl['sq'] = sq # пока заглушка
+        dic_pl['crd'] = tmplst
+        self.PLineCrd.append(dic_pl)
         
     def SwapPntLst(self, lst, dir):
-        reslst = []
+        reslst = {}
         if dir == 1:
-            SP = self.GetNordPnt(lst)
+            SP = self.GetNordPnt(lst['crd'])
         elif dir == 2:
-            SP = self.GetWestPnt(lst)
+            SP = self.GetWestPnt(lst['crd'])
         elif dir == 3:
-            SP = self.GetSouthPnt(lst)
+            SP = self.GetSouthPnt(lst['crd'])
         else:
-            SP = self.GetEastPnt(lst)
-        j = lst.index(SP)
-        lst1 = lst[j:len(lst)]
-        lst2 = lst[0:j ]
-        reslst.extend(lst1)
-        reslst.extend(lst2)
+            SP = self.GetEastPnt(lst['crd'])
+        j = lst['crd'].index(SP)
+        lst1 = lst['crd'][j:len(lst['crd'])]
+        lst2 = lst['crd'][0:j ]
+        lst3 = []
+        lst3.extend(lst1)
+        lst3.extend(lst2)
+        reslst['sq'] = lst['sq']
+        reslst['crd'] = lst3
         return reslst
         
     def SortPntList(self):
@@ -563,7 +562,7 @@ class main:
         nmbdir = 0  # Направление нумерации: 1 - от севера, 2 - от запада, 3 - от юга, 4 - от востока
         tmplst.extend(self.PLineCrd)
         for crdlst in tmplst:
-            for txy in crdlst:
+            for txy in crdlst['crd']:
                 if lxy.count(txy) == 0:
                     lxy.append(txy)
         NP = self.GetNordPnt(lxy)
@@ -576,32 +575,32 @@ class main:
             if deltay >= deltax: # Ориентация север - юг
                 nmbdir = 1
                 for crdlst in tmplst:
-                    if NP in crdlst:
+                    if NP in crdlst['crd']:
                         idx = tmplst.index(crdlst)
             else: # Ориентация запад - восток
                 nmbdir = 2
                 for crdlst in tmplst:
-                    if WP in crdlst:
+                    if WP in crdlst['crd']:
                         idx = tmplst.index(crdlst)
         elif self.RBVar.get() ==  2: # Принудительно от севера
             nmbdir = 1
             for crdlst in tmplst:
-                if NP in crdlst:
+                if NP in crdlst['crd']:
                     idx = tmplst.index(crdlst)
         elif self.RBVar.get() ==  3: # Принудительно от запада
             nmbdir = 2
             for crdlst in tmplst:
-                if WP in crdlst:
+                if WP in crdlst['crd']:
                     idx = tmplst.index(crdlst)
         elif self.RBVar.get() ==  4: # Принудительно от юга
             nmbdir = 3
             for crdlst in tmplst:
-                if SP in crdlst:
+                if SP in crdlst['crd']:
                     idx = tmplst.index(crdlst)
         else: # Принудительно от востока
             nmbdir = 4
             for crdlst in tmplst:
-                if EP in crdlst:
+                if EP in crdlst['crd']:
                     idx = tmplst.index(crdlst)
         # Берем первый контур по найденному индексу
         tmpcrd = self.SwapPntLst(tmplst.pop(idx), nmbdir)
@@ -611,9 +610,9 @@ class main:
             del lidx[0:len(lidx)]
             for tmpcrd in looplst:
                 idx = -1
-                m1 = set(tmpcrd)
+                m1 = set(tmpcrd['crd'])
                 for crdlst in tmplst:
-                    m2 = set(crdlst)
+                    m2 = set(crdlst['crd'])
                     if len(m1 & m2) > 0: # Если мощность пересечения больше нуля - соседний участок найден
                         idx = tmplst.index(crdlst)
                         if not idx in lidx:
@@ -627,7 +626,7 @@ class main:
             if (idx == -1) and (len(lidx) == 0): # если разрыв в трассе
                 del lxy[0:len(lxy)]
                 for crdlst in tmplst:
-                    for txy in crdlst:
+                    for txy in crdlst['crd']:
                         if lxy.count(txy) == 0:
                             lxy.append(txy)
                 # Крайние точки оставшихся участков
@@ -638,7 +637,7 @@ class main:
                 idx = 0
                 minlen = 100000
                 for tmpcrd in looplst:
-                    for txy in tmpcrd:
+                    for txy in tmpcrd['crd']:
                         nlen = self.Pifagor(txy, NP)
                         slen = self.Pifagor(txy, SP)
                         wlen = self.Pifagor(txy, WP)
@@ -656,7 +655,7 @@ class main:
                             minlen = elen
                             PP = EP
                 for crdlst in tmplst:
-                    if PP in crdlst: # если ближайшая точка принадлежит контуру - ближайший участок найден
+                    if PP in crdlst['crd']: # если ближайшая точка принадлежит контуру - ближайший участок найден
                         idx = tmplst.index(crdlst)
                 tmpcrd = self.SwapPntLst(tmplst.pop(idx), nmbdir)
                 fndlst.append(tmpcrd)
@@ -719,7 +718,8 @@ class main:
             if entity.Layer == LName:
                 ocnt[entity.EntityType] += 1
                 if entity.EntityType in (23,24):
-                    self.CollectCoord(entity.Coordinates)
+                    # также передать в collectcoord площадь
+                    self.CollectCoord(entity.Area, entity.Coordinates)
         for oc in xrange(1,len(ocnt)):
             if ocnt[oc] > 0:
                 self.master.entitys.insert(END, self.DicObjType[oc]+' ('+str(oc)+')'+' - '+str(ocnt[oc]))
@@ -792,18 +792,23 @@ class main:
         mltplr = self.DicScales.values().index(self.SclVar.get())
         if mltplr > 0:
             txthght *= mltplr
-        NP = self.GetNordPnt(lxy)
-        SP = self.GetSouthPnt(lxy)
-        WP = self.GetWestPnt(lxy)
-        EP = self.GetEastPnt(lxy)
+        NP = self.GetNordPnt(lxy['crd'])
+        SP = self.GetSouthPnt(lxy['crd'])
+        WP = self.GetWestPnt(lxy['crd'])
+        EP = self.GetEastPnt(lxy['crd'])
         x = (WP[0] + EP[0]) // 2
         y = (NP[1] + SP[1]) // 2
-        p1 = point(x,y)
+        p1 = point(x,y+txthght)
         ent = self.mspace.AddText(num, p1, txthght)
+        ent.Layer = lay
+        p1 = point(x,y-txthght)
+        s1 = str(trunc(lxy['sq']))
+        s2 = '{0:'+str(len(s1))+'.0f}'
+        ent = self.mspace.AddText(s2.format(lxy['sq']), p1, txthght)
         ent.Layer = lay
     
 if sys.platform.startswith('win'):
     root=Tk()
     main(root)
 else:
-    showerror(title = 'Error', message = 'Приложение разработано для платформы MS Windows!')
+    showerror(title = 'Error', message = u'Приложение разработано для платформы MS Windows!')
